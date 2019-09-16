c05d7b924748bea74fee3956ee23feb2277436fb
soobin28 <qwqw4860@naver.com>
2019-09-16 17:38:03 +0900

    190916최종

--- ReturnScroll/src/main/java/com/spring/returnscroll/HomeController.java
+++ ReturnScroll/src/main/java/com/spring/returnscroll/HomeController.java
@@ -17,23 +17,40 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController  {
 	  
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+
 		return "index";
+	}
+	
+	@RequestMapping(value = "/map", method = RequestMethod.GET)
+	public String map(Locale locale, Model model) {
+
+		return "map";
+	}
+	
+	@RequestMapping(value = "/chat", method = RequestMethod.GET)
+	public String chat(Locale locale, Model model) {
+
+		return "chat";
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(Locale locale, Model model) {
+
+		return "login";
+	}
+	
+	@RequestMapping(value = "/join", method = RequestMethod.GET)
+	public String join(Locale locale, Model model) {
+
+		return "join";
+	}
+	
+	@RequestMapping(value = "/mypage", method = RequestMethod.GET)
+	public String mypage(Locale locale, Model model) {
+
+		return "mypage";
 	}
 	
 }
