a282a42da873b8969e9eb625d2323776a34923eb
tjdwns784 <tjdwns784@naver.com>
2019-09-27 16:23:58 +0900

    19.09.27 최종

--- ReturnScroll/src/main/java/com/spring/returnscroll/Dao/UsersDao.java
+++ ReturnScroll/src/main/java/com/spring/returnscroll/Dao/UsersDao.java
@@ -12,6 +12,7 @@ public class UsersDao {
 	@Autowired
 	SqlSessionTemplate ss;
 
+	//메일에 가입한 이름 뜨게 하는거
 	public String mailId(String uid, String email) {
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("uid", uid);
@@ -19,13 +20,15 @@ public class UsersDao {
 		return ss.selectOne("users.mailId", map);
 	}
 
+	// 아이디 찾기
 	public String searchId(String uname, String phone) {
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("uname", uname);
 		map.put("phone", phone);
 		return ss.selectOne("users.searchId", map);
 	}
-
+	
+	// 비번 찾기
 	public String searchPassword(String uid, String email) {
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("uid", uid);
@@ -33,18 +36,22 @@ public class UsersDao {
 		return ss.selectOne("users.searchPassword", map);
 	}
 
+	// 회원가입
 	public int join(Map<String, Object> map) {
 		return ss.insert("users.insert", map);
 	}
 
+	// 로그인
 	public String login(Map<String, Object> map) {
 		return ss.selectOne("users.login", map);
 	}
 	
+	// 회원 수정
 	public int userUpdate(Map<String, Object> map) {
 		return ss.update("users.update", map);
 	}
 	
+	// 회원 탈퇴
 	public int userDelete(Map<String, Object> map) {
 		return ss.delete("users.delete", map);
 	}
